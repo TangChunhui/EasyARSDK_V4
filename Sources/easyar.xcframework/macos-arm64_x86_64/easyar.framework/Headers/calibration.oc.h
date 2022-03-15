8fb913c38e5605b0deec9ea57455622f133fbc6a
tangchunhui <tch@topjoycloud.com>
2022-03-15 10:19:34 +0800

    macos-arm64_x86_64

---
+++ Sources/easyar.xcframework/macos-arm64_x86_64/easyar.framework/Headers/calibration.oc.h
@@ -0,0 +1,23 @@
+﻿//=============================================================================================================================
+//
+// EasyAR Sense 4.5.0.9653-15c04a97e
+// Copyright (c) 2015-2022 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
+// EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
+// and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
+//
+//=============================================================================================================================
+
+#import "easyar/types.oc.h"
+
+/// <summary>
+/// CalibrationDownloader is used for download and update of calibration data in MotionTracker. The calibration data will only take effect after reallocation of MotionTracker.
+/// </summary>
+@interface easyar_CalibrationDownloader : easyar_RefBase
+
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+
++ (easyar_CalibrationDownloader *) create;
+- (void)download:(NSNumber *)timeoutMilliseconds callbackScheduler:(easyar_CallbackScheduler *)callbackScheduler onCompleted:(void (^)(easyar_CalibrationDownloadStatus status, NSString * error))onCompleted;
+
+@end
